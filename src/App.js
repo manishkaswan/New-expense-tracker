75585af37ab1f1086dd314d18e48814059c530e1
manish kumar <manishkaswan5@gmail.com>
2024-04-10 14:47:19 +0530

    Complete your profile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
 
-import './App.css';
-import SignUpFormPrint from './components/Signup/SignUpFormPrint';
-import Login from './components/login/Login';
+import "./App.css";
 
+import SignUpFormPrint from "./components/Signup/SignUpFormPrint";
+import Login from "./components/login/Login";
+// import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import { Switch, Route } from "react-router-dom";
+import Welcome from "./components/welcome/Welcome";
+import Profile from "./components/profile/Profile";
 function App() {
   return (
-    <>
-      <Login></Login>
-    </>
+    <Switch>
+      <Route path="/auth">
+        {" "}
+        <Login/>
+      </Route>{" "}
+      <Route path="/welcome">
+        <Welcome></Welcome>
+      </Route>
+      <Route path="/profile">
+        <Profile></Profile>
+      </Route>
+      <Route path="/">
+        <SignUpFormPrint></SignUpFormPrint>
+      </Route>
+    </Switch>
   );
 }
 
