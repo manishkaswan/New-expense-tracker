089697430fc5651555b429b0629c546450b9a3db
manish kumar <manishkaswan5@gmail.com>
2024-04-10 10:28:51 +0530

    Creating Signup Screen

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React, { Fragment } from 'react';
+
+import './App.css';
+import LoginFormPrint from './components/login/LoginFormPrint';
+
+function App() {
+  return (
+    <Fragment>
+      <LoginFormPrint/>
+    </Fragment>
+  );
+}
+
+export default App;
